264f7ce0c1b7cf74a1297abdfaf5a5b67c857a81
Henrik Karlsson <hk.henrik@gmail.com>
2015-08-20 16:56:11 +0200

    Rebranded mod

--- src/main/java/com/ineentho/forgedocr/ExampleMod.java
+++ src/main/java/com/ineentho/forgedocr/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.ineentho.forgedocr;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -8,8 +8,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "forgedocr";
+    public static final String VERSION = "0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
